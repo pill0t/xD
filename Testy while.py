9328386038baaf3be5ebff6d5ad78d5a3da15591
pill0t <93548961+pill0t@users.noreply.github.com>
2023-05-16 17:22:09 +0000

    Dodany liczik

--- Testy while.py
+++ Testy while.py
@@ -1,15 +1,17 @@
 import random
 my_number = random.randint(0,20)
 guess = -1
+trials = 0
 
 print('Guess my number')
 
 while guess != my_number:
 
     guess = int(input())
+    trials +=1
 
     if guess == my_number:
-        print("You're the best! It was", my_number)
+        print("You're the best! It was", my_number,"You needed", trials, "trials")
     elif guess>my_number:
         print("Sorry, my number is diffrent, it a little bit smaller", guess, "Try again!")
     else:
